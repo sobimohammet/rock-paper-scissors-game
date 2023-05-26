f1907364601e6049f3538f98cfaf66eac864d4f3
Sobi Mohammet <sobimohammet@gmail.com>
2023-05-26 05:11:53 +0100

    Edit script.js ie make game loop 5 rounds

--- script.js
+++ script.js
@@ -4,37 +4,50 @@ function getComputerChoice() {
     return (options[Math.floor(Math.random()*options.length)]).toLocaleLowerCase();
 }
 
-// get player choice
-function getPlayerChoice() {
-    let options = prompt(`Enter your selection`);
-    return options.toLocaleLowerCase();
-}
-
-//setting player and computer selections to variables
-let playerSelection = getPlayerChoice();
-let computerSelection = getComputerChoice();
-
+let playerScore = 0;
+let computerScore = 0;
 
 // single round function
 function playRound(playerSelection, computerSelection) {
 
-    playerSelection;
-    computerSelection;
-
-    // Game outcome statements
-    let playerWin = `You win! ${playerSelection} beats ${computerSelection}`;
-    let computerWin = `You Lose! ${computerSelection} beats ${playerSelection}`;
-    let drawGame = `It's a draw! Give it another shot`;
     if (playerSelection === computerSelection) {
-        console.log(drawGame);
+        return `It's a draw! Give it another shot`;
     } else if ((playerSelection === "rock" && computerSelection === "scissors") || 
                (playerSelection === "paper" && computerSelection === "rock") || 
                (playerSelection === "scissors" && computerSelection === "paper")) {
-        console.log(playerWin);
+        return `You win! ${playerSelection} beats ${computerSelection}`;
     } else if ((playerSelection === "scissors" && computerSelection === "rock") || 
                (playerSelection === "rock" && computerSelection === "paper") || 
                (playerSelection === "paper" && computerSelection === "scissors")) {
-        console.log(computerWin);
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
     } 
 }
-playRound(playerSelection, computerSelection);
+
+
+
+//looping playRound function for a 5 rounds game
+function game() {
+    for (i = 0; i < 5; i++) {
+        let playerSelection = prompt(`Enter your choice`).toLocaleLowerCase();
+        let computerSelection = getComputerChoice();
+        
+        function scoreTally() {
+            if (playRound(playerSelection, computerSelection) === `You win! ${playerSelection} beats ${computerSelection}`) {
+                playerScore = playerScore + 1;
+                return `You: ${playerScore} computer: ${computerScore}`;
+            } else if (playRound(playerSelection, computerSelection) === `You Lose! ${computerSelection} beats ${playerSelection}`) {
+                computerScore = computerScore + 1;
+                return `You: ${playerScore} computer: ${computerScore}`;
+            } else {
+                return `You: ${playerScore} Computer: ${computerScore}`;
+            }
+        }
+        
+        console.log(`${playRound(playerSelection, computerSelection)} score is ${scoreTally()}`);
+        
+    }
+    scoreTally();
+}
+
+
+game();
